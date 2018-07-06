e31bf9a6f83c7904b9e04ae1b243fe5f353725a9
Christian Riboldi <cmriboldi@gmail.com>
2018-07-06 15:42:34 -0600

    Adds the best file ever.

---
+++ MergeConflicts/TheBestFileEver.swift
@@ -0,0 +1,17 @@
+//
+//  TheBestFileEver.swift
+//  MergeConflicts
+//
+//  Created by Christian Riboldi on 7/6/18.
+//  Copyright © 2018 Christian Riboldi. All rights reserved.
+//
+
+import Foundation
+
+class ClassThatTookForever {
+    
+    func mostImportantFunction() -> Bool {
+        return true
+    }
+    
+}
