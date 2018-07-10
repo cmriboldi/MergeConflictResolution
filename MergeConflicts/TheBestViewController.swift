2cc0cbb4ef77fdf914991134368223f38a5821fc
Christian Riboldi <cmriboldi@gmail.com>
2018-07-10 15:04:42 -0600

    Adds TheBestViewController..

---
+++ MergeConflicts/TheBestViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TheBestViewController.swift
+//  MergeConflicts
+//
+//  Created by Christian Riboldi on 7/10/18.
+//  Copyright © 2018 Christian Riboldi. All rights reserved.
+//
+
+import UIKit
+
+class TheBestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func theMostImportantFunction() -> Bool {
+        return true
+    }
+
+}
